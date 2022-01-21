e7249c0fde22df7079a8265f440accd60fd8a30d
GMCINTO <89421618+GMCINTO@users.noreply.github.com>
2022-01-21 14:15:15 -0700

    Add files via upload

---
+++ learnio.cpp
@@ -0,0 +1,38 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+	int number1;
+	double number2;
+	char char1;
+	string string1;
+	string string2;
+
+	cout << "Enter number 1 ";
+		cin >> number1;
+
+	cout << "Enter number 2 " ;
+		cin >> number2;
+
+	cout << "Enter char 1 ";
+		cin >> number2;
+
+	cout << "Enter string 1 ";
+		cin >> number2;
+
+	cout << "Enter stirng 2 ";
+		cin >> number2;
+
+	cout << endl;
+	system("pause");
+	return 0;
+}
+
+/* PROBLEMS
+
+
+
+
+
+*/
